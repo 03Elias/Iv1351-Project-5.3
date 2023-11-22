3545b4528366d26a1bfa71c7ced229d127656168
03Elias <124251333+03Elias@users.noreply.github.com>
2023-11-22 16:06:14 +0100

    Update IV1351.sql

--- IV1351.sql
+++ IV1351.sql
@@ -1,4 +1,4 @@
--- Database: iv1351
+-- Database: iv1351 Participants: Elias Gaghlasian, Ammar Alzeno, Leo Långberg CINTE ÅK 2
 
 CREATE TYPE genre     AS ENUM('Classical', 'Jazz', 'Pop', 'Rock');
 CREATE TYPE instrumentals AS ENUM('Piano', 'Guitar', 'Bass', 'Drums', 'Violin', 'Saxophone', 'Trumpet', 'Flute');
@@ -48,29 +48,29 @@ CREATE TABLE instrument (
 CREATE TABLE lesson (    
   lesson_id SERIAL NOT NULL, 
   student_count INT NOT NULL DEFAULT 0,
-  min_students INT NOT NULL,
-  max_students INT NOT NULL,  
+  min_students INT,
+  max_students INT,  
   lesson_time TIMESTAMP NOT NULL,
   instructor_id SERIAL REFERENCES instructor(instructor_id), 
   PRIMARY KEY(lesson_id)
 );
 
 CREATE TABLE individual (
-  lesson_id SERIAL REFERENCES lesson(lesson_id),
+  lesson_id SERIAL REFERENCES lesson(lesson_id) ON DELETE CASCADE,
   level difficulty NOT NULL,
   instrument_id SERIAL REFERENCES instrument(instrument_id),
   PRIMARY KEY(lesson_id, instrument_id)  
 );
 
 CREATE TABLE grouplesson (
-  lesson_id SERIAL REFERENCES lesson(lesson_id),
+  lesson_id SERIAL REFERENCES lesson(lesson_id) ON DELETE CASCADE,
   level difficulty NOT NULL,
   instrument_id SERIAL REFERENCES instrument(instrument_id),
   PRIMARY KEY(lesson_id, instrument_id) 
 );
 
 CREATE TABLE ensamble (
-  lesson_id SERIAL REFERENCES lesson(lesson_id),
+  lesson_id SERIAL REFERENCES lesson(lesson_id) ON DELETE CASCADE,
   genres genre NOT NULL,
   PRIMARY KEY(lesson_id)
 );
@@ -109,7 +109,6 @@ CREATE TABLE in_stock (
 );
 
 
-
 --TRIGGERS--
 -- max 2 rented instruments (TRIGGER)
 CREATE OR REPLACE FUNCTION check_max_rented_instruments()
@@ -132,8 +131,6 @@ ON instrument_rental
 FOR EACH ROW
 EXECUTE FUNCTION check_max_rented_instruments();
 
-
-
 --if 'individual' then set min/max (TRIGGER)
 CREATE OR REPLACE FUNCTION set_default_students_for_individual()
 RETURNS TRIGGER AS $$
@@ -153,29 +150,11 @@ ON individual
 FOR EACH ROW
 EXECUTE FUNCTION set_default_students_for_individual();
 
-
-
---student_count updated from student_lesson_cross_reference (TRIGGER)
-CREATE OR REPLACE FUNCTION update_student_count()
-RETURNS TRIGGER AS $$
-BEGIN
-  UPDATE lesson
-  SET student_count = (
-    SELECT COUNT(student_id)
-    FROM student_lesson_cross_reference
-    WHERE lesson_id = NEW.lesson_id
-  )
-  WHERE lesson_id = NEW.lesson_id;
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
 CREATE TRIGGER update_student_count_trigger
-AFTER INSERT OR UPDATE OR DELETE
+AFTER INSERT OR UPDATE
 ON student_lesson_cross_reference
 FOR EACH ROW
 EXECUTE FUNCTION update_student_count();
-
 
 
 --Symmetrical Sibling inserts--
@@ -213,6 +192,66 @@ AFTER DELETE
 ON student_sibling
 FOR EACH ROW
 EXECUTE FUNCTION symmetrical_sibling_delete_trigger();
+
+
+--student_count updated from student_lesson_cross_reference (TRIGGER)
+CREATE OR REPLACE FUNCTION update_student_count()
+RETURNS TRIGGER AS $$
+BEGIN
+  UPDATE lesson
+  SET student_count = (
+    SELECT COUNT(slcr.student_id)
+    FROM student_lesson_cross_reference slcr
+    WHERE slcr.lesson_id = NEW.lesson_id
+  )
+  WHERE lesson_id = NEW.lesson_id;
+
+  IF (
+    SELECT COUNT(slcr.student_id)
+    FROM student_lesson_cross_reference slcr
+    WHERE slcr.lesson_id = NEW.lesson_id
+  ) > (SELECT max_students FROM lesson WHERE lesson_id = NEW.lesson_id) THEN
+    RAISE EXCEPTION 'Lesson is full. Cannot add more students.';
+  END IF;
+
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION time_available_booked_function()
+RETURNS TRIGGER AS $$
+DECLARE
+  t TIMESTAMP;  -- Declare a variable to store the result of the SELECT statement
+BEGIN
+  SELECT l.lesson_time
+  INTO t
+  FROM lesson l
+  WHERE l.lesson_id = NEW.lesson_id;
+
+  -- Check if there is a matching time_available in instructor_time_available
+  IF EXISTS (
+    SELECT 1
+    FROM instructor_time_available
+    WHERE time_available = t
+  ) THEN
+    -- If a match is found, delete the corresponding record in instructor_time_available
+    DELETE FROM instructor_time_available WHERE time_available = t;
+  ELSE
+    -- If no match is found, raise an exception
+    RAISE EXCEPTION 'Time not available for lesson_id: %', NEW.lesson_id;
+  END IF;
+
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+---individual makes use of the isntructor_time_available ---
+CREATE TRIGGER time_available_booked
+BEFORE INSERT
+ON individual
+FOR EACH ROW
+EXECUTE FUNCTION time_available_booked_function();
+
 
 
 
